af266a8a1679bc50d94d431cf45b160fbc79b0a9
s416007 <IchMAchs1009+?>
2022-01-24 21:38:46 +0100

    coffee is extra class for more organization

--- app/model/CoffeeFetcher.java
+++ app/model/CoffeeFetcher.java
@@ -2,6 +2,7 @@ package model;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import data.Coffee;
 import data.Ingredient;
 import play.db.Database;
 
@@ -20,36 +21,36 @@ public class CoffeeFetcher {
         this.db = db;
     }
 
-    public CoffeeFetcher.Coffee getCoffeeById(int id) {
+    public Coffee getCoffeeById(int id) {
         return db.withConnection(conn -> {
-            CoffeeFetcher.Coffee coffee = null;
+            Coffee coffee = null;
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Coffees WHERE idCoffees = ?");
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
                 List<Ingredient> ingredientList = getIngredientsByID(rs.getString("title"));
-                coffee = new CoffeeFetcher.Coffee(rs, ingredientList);
+                coffee = new Coffee(rs, ingredientList);
             }
             stmt.close();
             return coffee;
         });
     }
 
-    public CoffeeFetcher.Coffee getCoffeeById(String id) {
+    public Coffee getCoffeeById(String id) {
         return getCoffeeById(Integer.parseInt(id));
     }
 
 
-    public List<CoffeeFetcher.Coffee> getAllCoffees() {
+    public List<Coffee> getAllCoffees() {
         return db.withConnection(conn -> {
-            List<CoffeeFetcher.Coffee> coffees = new ArrayList<>();
+            List<Coffee> coffees = new ArrayList<>();
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Coffees");
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getString("title"));
                 List<Ingredient> ingredientList = getIngredientsByID(rs.getString("title"));
                 System.out.println(ingredientList);
-                CoffeeFetcher.Coffee coffee = new CoffeeFetcher.Coffee(rs, ingredientList);
+                Coffee coffee = new Coffee(rs, ingredientList);
                 coffees.add(coffee);
             }
             stmt.close();
@@ -71,88 +72,6 @@ public class CoffeeFetcher {
             stmt.close();
             return ingredients;
         });
-    }
-
-    public class Coffee {
-        private int id;
-        private String title;
-        private String description;
-        private int ingredientID;
-        private float price;
-        private String coffeeImgPath;
-        private List<Ingredient> ingredientList;
-
-        private Coffee(ResultSet rs, List <Ingredient> ingredients) throws SQLException {
-            this.id = rs.getInt("idCoffees");
-            this.title = rs.getString("title");
-            this.description = rs.getString("description");
-            this.ingredientID = rs.getInt("ingredients");
-            this.price = rs.getFloat("price");
-            this.coffeeImgPath = rs.getString("coffeeImgPath");
-            this.ingredientList = ingredients;
-        }
-
-
-        private Coffee(int id, String title, String description, int ingredientID, float price, String coffeeImgPath, List<Ingredient> ingredientList) {
-            this.id = id;
-            this.title = title;
-            this.description = description;
-            this.ingredientID = ingredientID;
-            this.price = price;
-            this.coffeeImgPath = coffeeImgPath;
-            this.ingredientList = ingredientList;
-        }
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public void setIngredients(int ingredientID) {
-            this.ingredientID = ingredientID;
-        }
-
-        public void setPrice(float price) {
-            this.price = price;
-        }
-
-        public void setCoffeeImgPath(String coffeeImgPath) {
-            this.coffeeImgPath = coffeeImgPath;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public int getIngredientID() {
-            return ingredientID;
-        }
-
-        public float getPrice() {
-            return price;
-        }
-
-        public String getCoffeeImgPath() {
-            return coffeeImgPath;
-        }
-
-        public List<Ingredient> getIngredientList() {
-            return ingredientList;
-        }
     }
 
 }
