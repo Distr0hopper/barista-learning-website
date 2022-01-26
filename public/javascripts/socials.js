e5f8524d414174a2b2cfc61ede8f6e3e1689cbb1
immerdieselisa <rieder.lisajohanna@gmail.com>
2022-01-26 12:56:12 +0100

    Socials page updated. Can send messages now, but scaling is off.

--- public/javascripts/socials.js
+++ public/javascripts/socials.js
@@ -4,6 +4,7 @@ function openChat(name, username, source){
     document.getElementById("header-avatar").src = source;
     document.getElementById("select-friend").style.display = "none";
     document.getElementById("chat-history").style.display = "block";
+    getMessages();
 }
 
 function openModal(){
@@ -17,4 +18,37 @@ function closeModal(){
     $('#friendModal').modal('hide');
 }
 
+function search(){
 
+}
+
+const messages = ["myself:Hi", "friend:Hello, how are you?"]
+
+function getMessages(){
+    $('#chat-list').empty();
+    for (let i = 0; i < messages.length; i++) {
+        let message = messages[i].split(":");
+        if(message[0]==="myself"){
+            createMessage("my-message", message[1]);
+        }else{
+            createMessage("their-message", message[1]);
+        }
+    }
+}
+
+function sendMessage(){
+    let message = document.getElementById("message");
+    createMessage("my-message", message.value);
+    message.value = '';
+}
+
+function createMessage(klasse, message){
+    let ul = document.getElementById("chat-list");
+    let li = document.createElement("li");
+    let div = document.createElement("div");
+    li.className = "clearfix";
+    div.className = klasse;
+    div.appendChild((document.createTextNode(message)));
+    li.appendChild(div);
+    ul.appendChild(li);
+}
