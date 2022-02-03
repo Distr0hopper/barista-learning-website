fe1880f96ecd36c89281b8f3ef066ac299114a50
2175 <ronja.fricke@stud-mail.uni-wuerzburg.de>
2022-02-03 17:15:26 +0100

    in Model 'Costumer'  added Pictures which fit with the correct costumers from the tiles Amount of pictures are shown with a random number to assign how many coffees have to be paid by whom.

--- public/javascripts/level3.js
+++ public/javascripts/level3.js
@@ -1,28 +1,23 @@
+
+async function addCoffee(){
+    $('#firstCoffee .coffePickerdiv').clone().appendTo('#moreCoffee');
+}
+async function removeCoffee(){
+    $('#moreCoffee .coffePickerdiv').last().remove();
+}
+
+
+
+
+
 async function loadModal() {
-    var gameModal = $('#gameModal3')
+    var gameModal = $('#gameModal2')
     gameModal.modal('show');
-    var timeleft = 10;
-    var currentHTMLText = document.querySelector("#modal-title").textContent;
-    var downloadTimer = setInterval(function () {
-        if (timeleft > 0) {
-            gameModal.find('.modal-title').text(currentHTMLText + " " + timeleft + ' seconds remaining');
-        } else if (timeleft < 0) {
-            // gameModal.modal("hide");
-            console.log('Vor Methodenaufruf')
-            closeGameModal();
-        } else {
-            clearInterval(downloadTimer);
-            gameModal.find('.modal-title').text(currentHTMLText + ' Finished');
-            closeGameModal();
-        }
-        timeleft -= 1;
-    }, 1000);
 
-    function closeGameModal() {
-        gameModal.modal("hide");
-        console.log('Ende');
-    }
-
+    /**
+     * CoffeesForGame is a class, that has sixCoffees in the Constructor
+     * it first generates a random Number
+     * then it creates randomSixCoffees and Titles*/
     class CoffeesForGame {
         constructor() {
             this.sixCoffees = ['Hallo']
@@ -34,7 +29,12 @@ async function loadModal() {
             return randomNumber
         }
 
-        //fetch random coffees from API
+        /**
+         * fetch random coffees from API
+         * then it searches with help of our randomnumber earlier a coffee in the API output and pushes that coffee to sixCoffees Array (line 60) and shortens the API output array at that index with splice
+         * then it saves the coffe into sixCoffees from the constructor
+         * save it to sessionstorage
+         * */
         async getRandomSixCoffees() {
             const coffeeAPI = 'https://api.sampleapis.com/coffee/hot';
             const res = await fetch(coffeeAPI);
@@ -57,6 +57,7 @@ async function loadModal() {
             window.sessionStorage.setItem("coffees", JSON.stringify(this.sixCoffees))
         }
 
+        /**getCoffeeTitles() returns a map with the titles from sixCoffees*/
         getCoffeeTitles() {
             console.log(this.sixCoffees)
             return this.sixCoffees.map(coffee => {
@@ -67,6 +68,10 @@ async function loadModal() {
         }
     }
 
+    /**
+     * Customers is a class with the Instructor sixCustomer Array and this.getRandomSixCustomers()
+     * it first generates a random Number
+     * then it creates randomSixCustomers and Images*/
     class Customers {
         constructor() {
             this.sixCustomers = []
@@ -78,35 +83,64 @@ async function loadModal() {
             return randomNumber
         }
 
+        /**gets Customers (later) and puts them into array*/
         getRandomSixCustomers() {
             const CustomerArray = []
             const sixCustomers = []
-            //TODO: Change path images
-            CustomerArray[0] = 'assets/images/Customers/black-woman.png'
-            CustomerArray[1] = 'assets/images/Customers/karen.png';
-            CustomerArray[2] = 'assets/images/Customers/old-woman.png';
-            CustomerArray[3] = 'assets/images/Customers/young-man.png';
-            CustomerArray[4] = 'assets/images/Customers/samurai-man.png';
-            CustomerArray[5] = 'assets/images/Customers/red-head-man.png';
-            CustomerArray[6] = 'assets/images/Customers/old-black-man.png';
-            CustomerArray[7] = 'assets/images/Customers/indian-woman.png';
+            CustomerArray[0] = '../assets/images/Customers/black-woman.png'
+            CustomerArray[1] = '../assets/images/Customers/karen.png';
+            CustomerArray[2] = '../assets/images/Customers/old-woman.png';
+            CustomerArray[3] = '../assets/images/Customers/young-man.png';
+            CustomerArray[4] = '../assets/images/Customers/samurai-man.png';
+            CustomerArray[5] = '../assets/images/Customers/red-head-man.png';
+            CustomerArray[6] = '../assets/images/Customers/old-black-man.png';
+            CustomerArray[7] = '../assets/images/Customers/indian-woman.png';
             // console.log(CustomerArray);
             for (let i = 0; i < 6; i++) {
                 const randomNumber = this.getRandomNumber(CustomerArray.length);
                 sixCustomers.push(CustomerArray.splice(randomNumber, 1)[0]);
-
             }
             console.log(sixCustomers)
             this.sixCustomers = sixCustomers;
             window.sessionStorage.setItem("customers", JSON.stringify(this.sixCustomers))
         }
 
+        /**Saves the Customer Images into map*/
         getCustomerImages() {
             return this.sixCustomers.map(customer => customer);
         }
     }
+    class CoffeeToBePayed {
+        constructor() {
+            this.getRandomCoffeeAmountNumber();
 
-    //put names in modal
+        }
+        getRandomCoffeeAmountNumber() {
+            const randomNumber = Math.floor(Math.random() * 5);
+            return randomNumber
+        }
+
+    }
+
+    const payedCoffeeNumber = new CoffeeToBePayed();
+    const numberOfPayedCoffeeByOneCostumer = payedCoffeeNumber.getRandomCoffeeAmountNumber();
+    console.log(payedCoffeeNumber);
+    document.getElementById("label").innerText= numberOfPayedCoffeeByOneCostumer.toString();
+
+    for (let i = 1; i <= numberOfPayedCoffeeByOneCostumer; ++i) {
+        if(numberOfPayedCoffeeByOneCostumer !== 1) {
+
+            $('.addingCostumer').clone().appendTo('#addingMoreCostumers');
+            i++;
+        }
+
+
+    }
+
+
+
+    /**
+     * put Coffeetitles in modal*/
     const coffeesForGame = new CoffeesForGame();
     await coffeesForGame.getRandomSixCoffees();
     const coffeeOrderCards = $('.card-text');
@@ -116,18 +150,36 @@ async function loadModal() {
         coffeeOrderCards[i].innerText = coffeeTitles[i];
     }
 
-    // add Pictures
+    /**put CustomerImages in to the tiles */
     const customerForGame = new Customers();
     await customerForGame.getRandomSixCustomers();
+
     const coffeeOrderCustomers = $('.card-img-top');
     const customerImages = customerForGame.getCustomerImages();
     console.log(customerImages);
     for (let i = 0; i < coffeeOrderCustomers.length; i++) {
         coffeeOrderCustomers[i].src = customerImages[i];
     }
+   /** put CustomerImages of the Payer in the correct modal position and the CustomerImages of the customers who are beeing payed for*/
+    const coffeeOrderCustomersPayer = $('.card-img-Person-paying');
+    const payerForGame = customerForGame.getCustomerImages();
+    console.log(payerForGame)
+    for (let i = 0; i < coffeeOrderCustomersPayer.length; i++) {
+        coffeeOrderCustomersPayer[i].src = payerForGame[i]
+    }
+
+    const customerBeingPayed = $('.card-img-Person-getting-payed')
+    const beingPayedFor = customerForGame.getCustomerImages();
+    for( let i = 0; i < customerBeingPayed.length; i++){
+        customerBeingPayed[i].src = beingPayedFor[i+1];
+    }
 
 
-    // function submitTotalPrice(){
 
 
-    window.addEventListener('load', loadModal)}
+
+
+
+
+}
+window.addEventListener('load', loadModal)
